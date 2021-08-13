5393a41c7fd52ee02e1a7214e45ab4ab560c4ac7
Ri4an <vadimguber7@gmail.com>
2021-08-13 23:57:40 +0600

    feat: add changing bg color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,13 @@
 import { Button } from '@material-ui/core'
 
-export const CounterButton = ({
-    text,
-    onClick,
-}: {
-    text: string
-    onClick: (e: any) => void
-}) => (
+export const CounterButton = (props: { text: string; onClick: () => void }) => (
     <Button
         variant='contained'
-        color={text === '+' ? 'primary' : 'secondary'}
-        onClick={onClick}
+        color={props.text === '+' ? 'primary' : 'secondary'}
+        onClick={() => {
+            props.onClick()
+        }}
     >
-        {text}
+        {props.text}
     </Button>
 )
