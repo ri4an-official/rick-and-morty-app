f694efe65380bcff99368ebdd7034e133a30294c
Ri4an <vadimguber7@gmail.com>
2021-08-19 00:43:27 +0600

    fix: change template app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { Profile } from './components/Profile'
 import './styles/main.scss'
-import { Counter } from './components/Counter'
 
 const App = () => (
     <div className='app'>
-        <img
-            src='https://i.ya-webdesign.com/images/rick-and-morty-family-png-8.png'
-            alt=''
-        />
-        <Counter />
+        <Profile />
     </div>
 )
 
