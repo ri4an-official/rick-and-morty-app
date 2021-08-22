03f6c8d4bdc67ea9fe8e3d8fbcf2cc8202ad8aee
Ri4an <vadimguber7@gmail.com>
2021-08-22 16:19:38 +0600

    fix: setup

--- src/components/Media.tsx
+++
@@ -1,7 +0,0 @@
-import closeRed from './../assets/svg/close-red.svg'
-export const Media = ({ src }: { src: string }) => (
-    <div>
-        <img src={src} className='photo' alt='my media' />
-        <img className='btn-close' src={closeRed} alt='close-btn' />
-    </div>
-)
