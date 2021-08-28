0fd9d7cd5bd191d3ced73690faa2c1fed35a3ccd
Ri4an <vadimguber7@gmail.com>
2021-08-29 01:30:15 +0600

    feat: add template for locations

---
+++ src/components/Locations/Locations.tsx
@@ -0,0 +1,3 @@
+export const Locations = () => {
+    return <div className='locations'></div>
+}
---
+++ src/components/Locations/SelectedLocation.tsx
@@ -0,0 +1,3 @@
+export const SelectedLocation = () => {
+    return <div className='selected-location'></div>
+}
