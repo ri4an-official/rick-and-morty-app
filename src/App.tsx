2bc403daad0575cfadc2f0011fe87d11fc39dcea
Ri4an <vadimguber7@gmail.com>
2021-08-29 01:29:06 +0600

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import './styles/main.sass'
-import title from './assets/svg/title.svg'
-import start from './assets/svg/start-screen.svg'
-const App = () => (
+import './styles/global.sass'
+// import title from './assets/svg/title.svg'
+import { Route, Switch } from 'react-router-dom'
+import { Characters } from './components/Characters/Characters'
+import { SelectedCharacter } from './components/Characters/SelectedCharacter'
+import { Locations } from './components/Locations/Locations'
+import { SelectedLocation } from './components/Locations/SelectedLocation'
+import { Episodes } from './components/Episodes/Episodes'
+import { SelectedEpisode } from './components/Episodes/SelectedEpisode'
+import { NotFound } from './components/base/NotFound'
+import { NavBar } from './components/NavBar'
+
+export const App = () => (
     <div className='app'>
-        <img className='title' src={title} alt='title' />
+        <main className='content'>
+            <Switch>
+                <Route exact path='/characters' component={Characters} />
+                <Route exact path='/characters/:id' component={SelectedCharacter} />
+                <Route exact path='/locations' component={Locations} />
+                <Route exact path='/locations/:id' component={SelectedLocation} />
+                <Route exact path='/episodes' component={Episodes} />
+                <Route exact path='/episodes/:id' component={SelectedEpisode} />
+                <Route exact component={NotFound} />
+            </Switch>
+        </main>
+        <NavBar />
     </div>
 )
-
-export default App
