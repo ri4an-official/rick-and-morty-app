4f8ab748b8b8981008a5c6826226aebd7d19ccda
Ri4an <vadimguber7@gmail.com>
2021-09-01 23:14:57 +0600

    fix: change class

--- src/components/base/icons/ArrowIcon.tsx
+++ src/components/base/icons/ArrowIcon.tsx
@@ -1,6 +1,6 @@
-import arrow from './../../assets/svg/arrow.svg'
+import arrow from './../../../assets/svg/arrow.svg'
 export const ArrowIcon = () => (
-    <span className='icon arrow-icon'>
+    <span className='icon arrow'>
         <img src={arrow} alt='arrow' />
     </span>
 )
