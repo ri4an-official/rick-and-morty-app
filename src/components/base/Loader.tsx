06a502854721b7a1aa37b0c54031838be2dc8b84
Ri4an <vadimguber7@gmail.com>
2021-09-01 23:14:45 +0600

    fix: change align

--- src/components/base/Loader.tsx
+++ src/components/base/Loader.tsx
@@ -1,6 +1,6 @@
 import loader from './../../assets/svg/loader.svg'
 export const Loader = () => (
-    <div className='center loader'>
+    <div className='loader'>
         <img src={loader} alt='loader' />
     </div>
 )
