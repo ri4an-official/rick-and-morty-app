80a3c700dddc0163ab7f4c3c26a0cc165503ac2b
Ri4an <vadimguber7@gmail.com>
2021-09-06 00:47:11 +0600

    feat: add seasons

--- src/components/Episodes/Episodes.tsx
+++ src/components/Episodes/Episodes.tsx
@@ -8,16 +8,29 @@ import { EpisodeItem } from './EpisodeItem'
 export const Episodes = observer(() => {
     useAsyncEffect(async () => {
         await episodes.getAll()
-    }, [episodes.list.length])
-    return episodes.list.length ? (
-        <div className='episodes'>
-            {episodes.list.map((e) => (
-                <Link to={`/episodes/${e.id}`}>
-                    <EpisodeItem key={e.id}>{e}</EpisodeItem>
-                </Link>
-            ))}
+    }, [episodes])
+    return (
+        <div>
+            <div className='seasons'>
+                {[1, 2, 3, 4, 5].map((s) => (
+                    <div
+                        onClick={async () => {
+                            await episodes.getAll()
+                            episodes.filterBySeason(s)
+                        }}
+                        className='season'
+                    >
+                        {s}
+                    </div>
+                ))}
+            </div>
+            <div className='episodes'>
+                {episodes.list.map((e) => (
+                    <Link to={`/episodes/${e.id}`}>
+                        <EpisodeItem key={e.id}>{e}</EpisodeItem>
+                    </Link>
+                ))}
+            </div>
         </div>
-    ) : (
-        <Loader />
     )
 })
