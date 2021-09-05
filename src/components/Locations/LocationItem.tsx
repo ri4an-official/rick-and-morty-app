dd5930c0d5c7e012f2760db39c92934eccddf901
Ri4an <vadimguber7@gmail.com>
2021-09-06 00:48:33 +0600

    fix: change desing

--- src/components/Locations/LocationItem.tsx
+++ src/components/Locations/LocationItem.tsx
@@ -4,7 +4,7 @@ export const LocationItem = ({ children }: { children: Location }) => (
     <div className='location-item'>
         <img src={children.imageName} alt='' />
         <div className='name'>{children.name}</div>
-        <div className='type-location'>{children.type} * </div>
+        <div className='type-location'>{children.type} - </div>
         <div className='location'>{children.measurements}</div>
     </div>
 )
