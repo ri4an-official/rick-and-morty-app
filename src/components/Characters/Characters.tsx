47f225864c282dadbe7f871cdf538fc740603c54
Ri4an <vadimguber7@gmail.com>
2021-09-09 00:15:11 +0600

    feat: add search, total characters

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -2,34 +2,24 @@ import { observer } from 'mobx-react-lite'
 import { Link } from 'react-router-dom'
 import useAsyncEffect from 'use-async-effect'
 import characters from '../../models/store/characters'
-import { Loader } from '../base/Loader'
 import { CharacterItem } from './CharacterItem'
-import filter from './../../assets/svg/filter.svg'
-import search from './../../assets/svg/search.svg'
-import { useState } from 'react'
+import { Search } from '../base/Search'
 export const Characters = observer(() => {
     useAsyncEffect(async () => {
         await characters.getAll()
-    }, [])
-    const [name, setName] = useState('')
+        await characters.getTotal()
+    }, [characters])
 
     return (
         <>
-            <div className='filter'>
-                <img className='seacrh-icon' src={search} alt='search' />
-                <input
-                    onChange={(e) => setName(e.currentTarget.value)}
-                    onKeyPress={async (e) =>
-                        e.key === 'Enter' &&
-                        (await characters.search({ name, status: 0, gender: 0 }))
-                    }
-                    placeholder='Найти персонажа'
-                />
-                <div className='vertical-line' />
-                <img className='filter-icon' src={filter} alt='filter' />
-            </div>
-
-            <div className='count'>ВСЕГО ПЕРСОНАЖЕЙ: {characters.list.length}</div>
+            <Search
+                onSubmit={async (query) =>
+                    await characters.search({ name: query, status: 0, gender: 0 })
+                }
+                placeholder='Найти персонажа'
+            />
+            {/* FIXME - fix total */}
+            <div className='count'>ВСЕГО ПЕРСОНАЖЕЙ: {characters.total}</div>
             <div className='characters'>
                 {characters.list.map((ch) => (
                     <Link to={`/characters/${ch.id}`}>
