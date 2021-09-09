a3cd32df98b8472ad40a214ef85852b340445e1b
Ri4an <vadimguber7@gmail.com>
2021-09-10 03:30:56 +0600

    feat: add template for settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,18 @@
-export const Settings = () => {
-    // TODO - fill component
-    return <div className='settings'></div>
-}
+import { observer } from 'mobx-react-lite'
+import user from '../models/store/user'
+import { withAuth } from './base/hocs/withAuth'
+import defaultAva from './../assets/svg/default-avatar.svg'
+export const Settings = observer(
+    withAuth(() => {
+        // TODO - fill component
+        return (
+            <div className='settings'>
+                <img className='ava' src={defaultAva} alt='' />
+                <div className='description'>
+                    <div className='name'>{user.current.fullName}</div>
+                    <div className='user-name'>{user.current.firstName}</div>
+                </div>
+            </div>
+        )
+    })
+)
