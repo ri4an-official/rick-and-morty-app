fc3605f59a6e1bb713a8b39004b4be76762ef16f
Ri4an <vadimguber7@gmail.com>
2021-09-11 19:25:00 +0600

    fix: delete not used import

--- src/components/base/hocs/withAuth.tsx
+++ src/components/base/hocs/withAuth.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import { Redirect } from 'react-router'
 import user from '../../../models/store/user'
-import { Login } from '../../Login'
 
 export const withAuth = (Component: FC<any>) => (props: any) =>
     user.auth ? <Component {...props} /> : <Redirect to={'/login'} />
