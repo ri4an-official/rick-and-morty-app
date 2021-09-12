3e1b8c239e2e4ba8c9ff45006c3ad0fd2d067162
Ri4an <vadimguber7@gmail.com>
2021-09-12 23:26:49 +0600

    feat: add check

--- src/components/Locations/LocationItem.tsx
+++ src/components/Locations/LocationItem.tsx
@@ -1,10 +1,17 @@
 import { Location } from '../../models/types/Location'
+import { NoImageIcon } from '../base/icons/NoImageIcon'
+import { PointIcon } from '../base/icons/PointIcon'
 
 export const LocationItem = ({ children }: { children: Location }) => (
     <div className='location-item'>
-        <img src={children.imageName} alt='' />
-        <div className='name'>{children.name}</div>
-        <div className='type-location'>{children.type} - </div>
-        <div className='location'>{children.measurements}</div>
+        {children.imageName ? <img src={children.imageName} alt='' /> : <NoImageIcon />}
+        <div className='container'>
+            <div className='name'>{children.name || '???'}</div>
+            <div className='type-location'>
+                {children.type || '???'}
+                <PointIcon />
+            </div>
+            <div className='location'>{children.measurements || '???'}</div>
+        </div>
     </div>
 )
