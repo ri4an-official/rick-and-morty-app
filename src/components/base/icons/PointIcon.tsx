c04750d05cdacd5c540eadd6d5852244d1dee894
Ri4an <vadimguber7@gmail.com>
2021-09-12 23:24:29 +0600

    feat: svg components

---
+++ src/components/base/icons/PointIcon.tsx
@@ -0,0 +1,12 @@
+export const PointIcon = () => (
+    <svg
+        className='point'
+        width='2'
+        height='2'
+        viewBox='0 0 2 2'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <circle cx='1' cy='1' r='1' fill='#6E798C' />
+    </svg>
+)
