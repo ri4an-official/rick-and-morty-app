5fa7fbb28602d91cdeb8dba7dc09356c4415c733
Ri4an <vadimguber7@gmail.com>
2021-09-12 23:27:49 +0600

    feat: delete same characters

--- src/models/store/characters.ts
+++ src/models/store/characters.ts
@@ -12,8 +12,13 @@ class Characters {
     async getTotal() {
         this.total = await characters.getTotal()
     }
-    async getAll(page = 1, pageSize = 20) {
+    async getAll(page = 1, pageSize = 15) {
         this.list = [...this.list, ...(await characters.getAll(page, pageSize))]
+        this.list.forEach(
+            (ch, ind) =>
+                this.list.filter((ch2) => ch2.id === ch.id).length > 1 &&
+                this.list.splice(ind, 1)
+        )
     }
     async search(options: { name: string; status: Status; gender: Gender }) {
         this.list = await characters.search({ ...options })
