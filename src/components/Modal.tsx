ad9c9a51daf53ab9d2d640cb8d92079d17515495
Ri4an <vadimguber7@gmail.com>
2021-09-17 23:30:38 +0600

    fix: content from string to JSX

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,19 @@
+import { FC } from 'react'
+
 export const Modal = (props: {
     visible: boolean
     onClose: () => void
     title: string
-    content: string
+    Content: FC<{ onClose: () => void }>
 }) =>
     props.visible ? (
         <div className='modal'>
             <div className='dialog' onClick={(e) => e.stopPropagation()}>
                 <div className='header'>
-                    <div className='title'>{props.title}</div>
+                    <div className='title-modal'>{props.title}</div>
                 </div>
                 <div className='body'>
-                    <div className='content'>{props.content}</div>
-                    <button onClick={props.onClose} className='btn-close'>
-                        OK
-                    </button>
+                    <props.Content onClose={props.onClose} />
                 </div>
             </div>
         </div>
