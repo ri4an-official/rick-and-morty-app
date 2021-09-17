0087785b8602b762356e326380871d9211ba9000
Ri4an <vadimguber7@gmail.com>
2021-09-17 23:31:28 +0600

    feat: add template modal changing theme

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,39 +5,62 @@ import defaultAva from './../assets/svg/default-avatar.svg'
 import pallete from './../assets/svg/color-pallete.svg'
 import { ArrowIcon } from '../common/icons/ArrowIcon'
 import { version } from '../common/version/index.json'
-// TODO- add theme, edit profile
+import { useState } from 'react'
+import { Modal } from './Modal'
+// TODO- add theme
 export const Settings = observer(
-    withAuth(() => (
-        <div className='settings'>
-            <div className='profile'>
-                <img className='ava' src={defaultAva} alt='' />
-                <div className='description'>
-                    <div className='name'>{user.current.fullName}</div>
-                    <div className='user-name'>{user.current.firstName}</div>
+    withAuth(() => {
+        const [isVisible, setIsVisible] = useState(false)
+        return (
+            <div className='settings'>
+                <div className='profile'>
+                    <img className='ava' src={defaultAva} alt='' />
+                    <div className='description'>
+                        <div className='name'>{user.current.fullName}</div>
+                        <div className='user-name'>{user.current.firstName}</div>
+                    </div>
                 </div>
-            </div>
-            <div className='center'>
-                <button className='edit'>Редактировать</button>
-            </div>
-            <hr />
-            <div className='pallete-label'>ВНЕШНИЙ ВИД</div>
-            <div className='theme'>
-                <img className='pallete' src={pallete} alt='' />
-                <div className='about'>
-                    <div className='name'>Темная тема</div>
-                    <div className='status'>Включена</div>
+                <div className='center'>
+                    <button className='edit'>Редактировать</button>
+                </div>
+                <hr />
+                <div className='pallete-label'>ВНЕШНИЙ ВИД</div>
+                <div className='theme' onClick={() => setIsVisible(true)}>
+                    <Modal
+                        onClose={() => setIsVisible(false)}
+                        visible={isVisible}
+                        title='Темная тема'
+                        Content={({ onClose }) => (
+                            <>
+                                <select name='' id=''>
+                                    <option value='Выключена'></option>
+                                    <option value='Включена'></option>
+                                    <option value='Следовать настройкам системы'></option>
+                                    <option value='В режиме энергосбережения'></option>
+                                </select>
+                                <button className='btn-cancel' onClick={onClose}>
+                                    ОТМЕНА
+                                </button>
+                            </>
+                        )}
+                    />
+                    <img className='pallete' src={pallete} alt='' />
+                    <div className='about'>
+                        <div className='name'>Темная тема</div>
+                        <div className='status'>Включена</div>
+                    </div>
                     <ArrowIcon />
                 </div>
+                <hr />
+                <div className='about'>О ПРИЛОЖЕНИИ</div>
+                <p className='about-text'>
+                    Зигерионцы помещают Джерри и Рика в симуляцию, чтобы узнать секрет
+                    изготовления концен-трирован- ной темной материи.
+                </p>
+                <hr />
+                <div className='version'>ВЕРСИЯ ПРИЛОЖЕНИЯ</div>
+                <p className='version-value'>Rick & Morty v{version}</p>
             </div>
-            <hr />
-            <div className='about'>О ПРИЛОЖЕНИИ</div>
-            <p className='about-text'>
-                Зигерионцы помещают Джерри и Рика в симуляцию, чтобы узнать секрет
-                изготовления концен-трирован- ной темной материи.
-            </p>
-            <hr />
-            <div className='version'>ВЕРСИЯ ПРИЛОЖЕНИЯ</div>
-            <p className='version-value'>Rick & Morty v{version}</p>
-        </div>
-    ))
+        )
+    })
 )
