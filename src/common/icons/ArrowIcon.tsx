89aca15b4dca50c7bbfac15a18bb6aa0b879eb10
Ri4an <vadimguber7@gmail.com>
2021-09-24 14:41:58 +0600

    feat: add new icons

--- src/common/icons/ArrowIcon.tsx
+++ src/common/icons/ArrowIcon.tsx
@@ -1,2 +1,18 @@
-import arrow from './../../assets/svg/arrow.svg'
-export const ArrowIcon = () => <img className='arrow-icon' src={arrow} alt='arrow' />
+export const ArrowIcon = () => (
+    <svg
+        className='arrow-icon'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M10 16L14 12L10 8'
+            stroke='white'
+            stroke-width='1.5'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+        />
+    </svg>
+)
